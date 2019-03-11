23dc1e8b957141908fcd364f0af5a42b26daf649
Kenny Henbest <lekenbest@gmail.com>
2019-03-11 13:15:12 -0600

    built

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: '/insectopedia/',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
